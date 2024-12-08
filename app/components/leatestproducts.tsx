51dac4f5fdfd4d9e2a94d5a9c3560499bf46cf7f
shaheryarshah <160542494+shaheryarshah@users.noreply.github.com>
2024-12-08 09:08:50 -0800

    Update leatestproducts.tsx

--- app/components/leatestproducts.tsx
+++ app/components/leatestproducts.tsx
@@ -346,7 +346,7 @@ export default function Leatestproducts() {
         </div>
         <div className="p-4  lg:w-1/4 md:w-1/2">
           <div className="h-full flex flex-col items-center text-center">
-            <img
+            <image
               alt="team"
               className="flex-shrink-0 rounded-lg w-full h-100 object-cover object-center mb-4"
               src="/lp6.png"
