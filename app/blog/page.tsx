14bc0ecd6c5b71f7ab0447cf4e9a34e7bf3e5bc9
shaheryarshah <160542494+shaheryarshah@users.noreply.github.com>
2024-12-08 10:00:02 -0800

    Update page.tsx

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -5,7 +5,7 @@ const ShoppingCart = () => {
         <h1 className="text-4xl font-bold text-center mb-8">Blog Page</h1>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
           <div className="bg-white rounded-lg shadow-md overflow-hidden">
-            <image
+            <Image
               src="/blog.png"
               alt="Blog Image"
               className="w-full h-48 object-cover"
