5aee481f0cb86431a486670418de19d0360f5f8a
shaheryarshah <160542494+shaheryarshah@users.noreply.github.com>
2024-12-08 12:15:28 -0800

    Update page.tsx

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -1,7 +1,7 @@
 
 const Shop = () => {
     return (
-        <div className="flex flex-col md:flex-row justify-center md:justify-between p-4 space-y-4 md:space-y-0">
+        <div>
 shoping
         </div>
     );
