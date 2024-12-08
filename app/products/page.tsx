7d792dd7b8d64b18a485508587e7c592f6ec6cfa
shaheryarshah <160542494+shaheryarshah@users.noreply.github.com>
2024-12-08 12:11:26 -0800

    Update page.tsx

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,1 +1,11 @@
 
+
+const Product = () => {
+    return (
+        <div className="flex flex-col md:flex-row justify-center md:justify-between p-4 space-y-4 md:space-y-0">
+discount
+        </div>
+    );
+};
+
+export default Product;
